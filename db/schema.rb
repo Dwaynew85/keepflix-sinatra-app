b0bec4ee7636df7aa6cb1a1efd8d5b12e37f52c8
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-08-20 07:51:02 +0000

    ran migration

--- db/schema.rb
+++ db/schema.rb
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 20200820054313) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.string   "username"
+    t.string   "email"
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
