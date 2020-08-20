44d26abbdc9c1bf23358cf73528443dc4145c524
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-08-20 07:24:05 +0000

    added to migration

--- db/migrate/20200820054313_create_movies.rb
+++ db/migrate/20200820054313_create_movies.rb
@@ -2,6 +2,8 @@ class CreateMovies < ActiveRecord::Migration
   def change
     create_table :movies do |t|
       t.string :title
+      t.string :stars
+      t.string :trailer_link
       t.string :genres
       t.string :rating
       t.string :runtime
