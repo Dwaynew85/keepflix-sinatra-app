b13566008032cd7ae51b1329cb91423851482d31
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-08-21 08:52:32 +0000

    added remaining routes along with messages for views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   get '/login' do
-    erb :login
+    erb :'/users/login'
   end
 
   post '/login' do
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
       redirect "/users/#{user.id}"
     else
       # create error message
-      redirect '/login'
+      redirect '/uses/login'
     end
   end
 
@@ -20,8 +20,20 @@ class UsersController < ApplicationController
     erb :'/users/show' # create users show page displaying greeting by name and listing all movies as links.
   end
 
-  get 'signup' do
-    erb :'/users/signup' #
+  get '/signup' do
+    erb :'/users/signup'
+  end
+
+  post '/users' do
+    @user = User.create(params)
+    session[:user_id] = @user.id
+    redirect "/users/#{@user.id}"
+  end
+
+  get '/logout' do
+    session.clear
+    # add logout message
+    redirect '/'
   end
 
 end
