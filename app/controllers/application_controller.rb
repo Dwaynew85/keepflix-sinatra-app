b5320d07a069095c62c21bfee495d91a2d851cc3
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-08-24 08:29:32 +0000

    registered sinatra flash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
     enable :sessions
     set :session_secret, "my_bizness_your_fassness"
+    register Sinatra::Flash
   end
 
   get "/" do
