1235bc0ecb20f067021a6622c73a01c4a77335f6
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-09-02 18:37:11 +0000

    fixed issue with breaking if there's no movie image

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,6 +5,7 @@ class Scraper
   def self.scrape_movie(link)
     movie_details = {}
     mov = Nokogiri::HTML(open(link))
+    # binding.pry
     movie_details[:title] = mov.css(".title_wrapper h1").children.first.text
     stars = mov.css(".credit_summary_item").last.css("a").children.collect {|star| star.text}
     stars.pop if stars.last.include?("See full cast")
@@ -14,7 +15,7 @@ class Scraper
     movie_details[:runtime] = mov.css(".subtext time").text.strip
     movie_details[:release_date] = mov.css(".subtext a").last.text.strip
     movie_details[:summary] = mov.css(".summary_text").text.strip
-    movie_details[:image_url] = mov.css(".poster img").attribute("src").value
+    movie_details[:image_url] = mov.css(".poster img").attribute("src").value if !mov.css(".poster img").empty?
     unless mov.css(".slate a").first == nil
       movie_details[:trailer_link] = "https://www.imdb.com/" + mov.css(".slate a").first.attributes["href"].value
     else
