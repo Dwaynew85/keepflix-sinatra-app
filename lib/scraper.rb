fbfa15ba6b6ba37a10ffaa43bd4993242c03808d
Dwaynew85 <mr_dwayne_w@yahoo.com>
2020-09-05 01:45:54 -0400

    movies will be saved regardless of if all fields are filled out

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,13 +5,12 @@ class Scraper
   def self.scrape_movie(link)
     movie_details = {}
     mov = Nokogiri::HTML(open(link))
-    # binding.pry
     movie_details[:title] = mov.css(".title_wrapper h1").children.first.text
     stars = mov.css(".credit_summary_item").last.css("a").children.collect {|star| star.text}
     stars.pop if stars.last.include?("See full cast")
     movie_details[:stars] = stars
     movie_details[:genres] = mov.css(".subtext a").map {|genre| genre.text unless genre.text =~ /\d/}.compact
-    movie_details[:rating] = mov.css(".subtext").children.first.text.strip
+    movie_details[:rating] = mov.css(".subtext").children.first.text.strip if !mov.css(".subtext").children.first.text.strip.empty? 
     movie_details[:runtime] = mov.css(".subtext time").text.strip
     movie_details[:release_date] = mov.css(".subtext a").last.text.strip
     movie_details[:summary] = mov.css(".summary_text").text.strip
